2043ccae3b16c43e847c2c78170be8bf213fb32f
Utkarsh Kamble <utkarsh.ipaypro@gmail.com>
2025-06-29 13:14:16 +0530

    latest commit

--- UserDetails.js
+++ UserDetails.js
@@ -1,11 +1,19 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const UserDetailSchema = new mongoose.Schema({
-    name:String,
-    email:{type:String, unique:true},
-    mobile:String,
-    password:String
-},{
-    collection:"UserInfo"
-})
-mongoose.model("UserInfo", UserDetailSchema)
+const UserDetailSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    mobile: String,
+    password: String,
+    image:String,
+    gender:String,
+    profession:String
+    
+  
+  },
+  {
+    collection: "UserInfo",
+  }
+);
+mongoose.model("UserInfo", UserDetailSchema);
--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-app.use(express.json())
-const bcrypt = require("bcrypt")
+app.use(express.json());
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const mongoUrl =
   "mongodb+srv://utkarsh1172:admin@cluster0.zpbhjep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-
+const JWT_SECRET =
+  "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jdsds039[]]pou89ywe";
 mongoose
   .connect(mongoUrl)
   .then(() => {
@@ -16,35 +18,93 @@ mongoose
   .catch((e) => {
     console.log(e);
   });
+require("./UserDetails");
+const User = mongoose.model("UserInfo");
 
-  require('./UserDetails')
-  const User=mongoose.model("UserInfo")
 app.get("/", (req, res) => {
-  res.send({ status: "Started not startdd yet mavcddn" });
+  res.send({ status: "Started" });
 });
 
-app.post('/register', async(req, res) =>{
-       const {name,email,mobile,password} = req.body;
+app.post("/register", async (req, res) => {
+  const { name, email, mobile, password } = req.body;
+  console.log(req.body);
 
-       const oldUser = await User.findOne({email:email})
+  const oldUser = await User.findOne({ email: email });
 
-       if(oldUser) {
-        return res.send({data: "User already existes!!"})
-       }
+  if (oldUser) {
+    return res.send({ data: "User already exists!!" });
+  }
+  const encryptedPassword = await bcrypt.hash(password, 10);
 
-       const encryptedPassword = await bcrypt.hash(password, 10)
-       try{
-        await User.create({
-            name:name,
-            email:email,
-            mobile,
-            password:encryptedPassword,
-        });
-        res.send({status:"ok", data:"User Created"})
-       }catch (error) {
-        res.send({status:"error", data:error})
-       }
-})
+  try {
+    await User.create({
+      name: name,
+      email: email,
+      mobile,
+      password: encryptedPassword,
+    });
+    res.send({ status: "ok", data: "User Created" });
+  } catch (error) {
+    res.send({ status: "error", data: error });
+  }
+});
+
+app.post("/login-user", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  const oldUser = await User.findOne({ email: email });
+
+  if (!oldUser) {
+    return res.send({ data: "User doesn't exists!!" });
+  }
+
+  if (await bcrypt.compare(password, oldUser.password)) {
+    const token = jwt.sign({ email: oldUser.email }, JWT_SECRET);
+    console.log(token);
+    if (res.status(201)) {
+      return res.send({ status: "ok", data: token });
+    } else {
+      return res.send({ error: "error" });
+    }
+  }
+});
+
+app.post("/userdata", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    const useremail = user.email;
+
+    User.findOne({ email: useremail }).then((data) => {
+      return res.send({ status: "Ok", data: data });
+    });
+  } catch (error) {
+    return res.send({ error: error });
+  }
+});
+
+app.post("/update-user", async (req, res) => {
+  const { name, email, mobile, image, gender, profession } = req.body;
+  console.log(req.body);
+  try {
+    await User.updateOne(
+      { email: email },
+      {
+        $set: {
+          name,
+          mobile,
+          image,
+          gender,
+          profession,
+        },
+      }
+    );
+    res.send({status:"Ok",data:"Updated"})
+  } catch (error) {
+    return res.send({ error: error });
+  }
+});
+
 app.listen(5001, () => {
   console.log("Node js server started.");
 });
